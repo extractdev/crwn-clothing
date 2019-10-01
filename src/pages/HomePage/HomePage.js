0ca642644f12eba70aa5e70f5b7966bfc447069c
AaronSmith <aaron@webroi.com.au>
2019-10-01 14:29:47 +1000

    Set up directory menu components and style menu items

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Directory from '../../components/Directory/Directory'
+import './style.scss'
+
+
+
+export default () => (
+    <div className="homepage">
+        <Directory />
+    </div>
+)
